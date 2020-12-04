c7cf6469cf7805d6713765484cba45e8f046e853
hannahchiu <lleyhan08@gmail.com>
2020-12-04 16:32:05 +0800

    add generation model of Romance of the Three Kingdoms

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import torch
 
 tokenizer = AutoTokenizer.from_pretrained('bert-base-chinese')
 model = AutoModelForCausalLM.from_pretrained('ckiplab/gpt2-base-chinese')
+model2 = AutoModelForCausalLM.from_pretrained('output5/checkpoint-3000')
 max_input_length = 50
 max_output_length = 50
 
@@ -40,11 +41,21 @@ def index2():
 @app.route('/three', methods=['POST'])
 def upload2():
     input_text = request.values['news']
-    output = "三國演義"
+    input_text = input_text.replace("，", ",")
+    # input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    # no [CLS] & [SEP]
+    tokenized_tokens = tokenizer.tokenize(input_text)
+    input_ids = tokenizer.convert_tokens_to_ids(tokenized_tokens)
+    input_ids = torch.tensor([input_ids])
+    previous_input = input_ids[:, :max(len(input_ids[0]) - max_input_length, 0)]
+    input_ids = input_ids[:, -max_input_length:]
+    output = model2.generate(input_ids, max_length=len(input_ids[0]) + max_output_length, repetition_penalty=1.2)
+    output = tokenizer.decode(torch.cat([previous_input[0], output[0]]))
+    output = output.replace(" ", "").replace("[CLS]", "").replace("[SEP]", "").replace("[UNK]", "").replace(",", "，")
     return render_template("index.html", res=output)
 
 
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.jinja_env.auto_reload = True
-    app.run(host='0.0.0.0', port=9000, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=8000, threaded=True, debug=True)
